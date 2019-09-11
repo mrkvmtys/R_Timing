b7d68cdd60dad092106abeec5630af798227e7cb
mrkvmtys <40655916+mrkvmtys@users.noreply.github.com>
2019-09-11 11:17:07 +0200

    Add files via upload
    Simple timing script and index builds for data.table

---
+++ R_timing_computation_time.R
@@ -0,0 +1,37 @@
+# benchmarking processing times in R
+library(data.table)
+library(dplyr)
+library(plyr)
+
+# timing function
+time <- function(...) {
+  time_measurement <- system.time(eval(...))
+  time_measurement[["user.self"]]
+}
+
+# run same code n times to get min / max / mean runtimes
+benchmark <- function(..., n = 100) {
+  times <- replicate(n, ...)
+  c(min = min(times), max = max(times), mean = mean(times))
+}
+
+
+# timing my slow join
+time(merge(x = final, y = mapping[, c("SepaCreditorName","SepaCreditorIBAN", "trCategory")], by = c("SepaCreditorName","SepaCreditorIBAN"), all.x = TRUE)
+)
+
+# set index key on data tables to see whether there's room for improvement (1k rows)
+setkey(mapping, SepaCreditorIBAN, SepaCreditorName)
+mapping <- as.data.table(mapping)
+key(mapping)
+
+# set index on final (120k rows)
+setkey(final, SepaCreditorIBAN, SepaCreditorName)
+final <- as.data.table(final)
+key(final)
+
+# get benchmarked times
+benchmark({
+  time(final2 <- merge(x = final, y = mapping[, c("SepaCreditorName","SepaCreditorIBAN", "trCategory")], by = c("SepaCreditorName","SepaCreditorIBAN"), all.x = TRUE)
+  )
+})
